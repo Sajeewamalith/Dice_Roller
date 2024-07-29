43ffe96bfb9ee950abdc04a533219dc273c6638f
Sajeewa Malith <sajeewamalith0219@gmail.com>
2024-07-29 17:20:08 +0530

    efficiently android code

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -13,6 +13,9 @@ import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage:ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -28,10 +31,11 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this,"Button Clicked",Toast.LENGTH_SHORT).show()
             rollDice()
         }
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
-        val diceImage:ImageView = findViewById(R.id.dice_image)
+
         val randomInt = Random.nextInt(6)+1
         val drawableResource = when(randomInt){
             1-> R.drawable.dice_1
