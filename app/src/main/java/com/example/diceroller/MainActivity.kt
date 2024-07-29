4b6476548f1540a1d49bba2330c6363f8273fa03
Sajeewa Malith <sajeewamalith0219@gmail.com>
2024-07-29 16:48:34 +0530

    Handle dice roll and display result - Generating a random number between 1 and 6 to simulate a dice roll. - Updating an ImageView to display the corresponding dice image based on the roll result.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,7 +26,22 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
             Toast.makeText(this,"Button Clicked",Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val diceImage:ImageView = findViewById(R.id.dice_image)
+        val randomInt = Random.nextInt(6)+1
+        val drawableResource = when(randomInt){
+            1-> R.drawable.dice_1
+            2-> R.drawable.dice_2
+            3-> R.drawable.dice_3
+            4-> R.drawable.dice_4
+            5-> R.drawable.dice_5
+            else->R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
 
